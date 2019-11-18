db4e9d520a2a1091d562e1672ffdc141c2d0a7f7
Brenda Joshua <brendajosh.bj@gmail.com>
2019-11-18 11:05:45 +0300

    set up server

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const server = express();
+server.use(helmet());
+const port = process.env.PORT;
+const friends = [
+   {id:1, name:'Ifiok'},
+   {id:2, name:'Jill'},
+   {id:3, name:'Joe'},
+];
+server.use(express.json());
+server.use(cors());
+server.get('/', (req, res) => {
+res.send("<h1>Welcome!</h1>")
+})
+server.get('/api/friends', (req, res, next) => {
+ res.json(friends);
+});
+server.listen(port, () => {
+   console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
+})
